68aeb0d874ffe93cdb2316a03fd39aa524930047
Robbie Pallas <robbie.pallas@piksel.com>
2019-10-16 22:04:32 +0100

    refactor tests

--- test/client/api/api.test.js
+++ test/client/api/api.test.js
@@ -1,7 +1,7 @@
 const Api = require('../../../src/client/api')
 const Fakes = require('../../helpers/fakes')
 
-let mockClient
+let mockClient, api
 
 describe('Api', () => {
   describe('when the request is successful', () => {
@@ -10,22 +10,23 @@ describe('Api', () => {
         get: Fakes.mock(Fakes.responses.OK),
         post: Fakes.mock(Fakes.responses.OK)
       }
+      api = Api('dummy-token', mockClient)
     })
 
     it('makes requests with an authentication token', async () => {
-      await Api('dummy-token', mockClient).makeRequest('GET', 'test')
-      
+      await api.makeRequest('GET', 'test')
+
       expect(mockClient.get.mock.calls[0][1].headers.Authorization).toEqual('Bearer dummy-token')
     })
 
     it('makes requests with correct path and method', async () => {
-      await Api('dummy-token', mockClient).makeRequest('POST', 'test')
+      await api.makeRequest('POST', 'test')
 
       expect(mockClient.post.mock.calls[0][0]).toEqual('test')
     })
 
     it('makes requests with correct options', async () => {
-      await Api('dummy-token', mockClient).makeRequest('POST', 'test', {
+      await api.makeRequest('POST', 'test', {
         query: 'param1=test',
         baseUrl: 'foo/'
       })
@@ -40,10 +41,11 @@ describe('Api', () => {
       mockClient = {
         get: Fakes.throws()
       }
+      api = Api('dummy-token', mockClient)
     })
 
     it('throws an error with correct message', async () => {
-      const makeRequest = Api('dummy-token', mockClient).makeRequest('get', 'test')
+      const makeRequest = api.makeRequest('get', 'test')
 
       await expect(makeRequest).rejects.toThrow('Http Client', 'trouble at t mill!')
     })
